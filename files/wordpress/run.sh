628b7a59c4cfe02735a266b97e193c8f2dc31167
SeungJoonChoi <tmdwns0845@naver.com>
2023-02-09 15:46:50 +0900

    Set up wordpress image

--- files/wordpress/run.sh
+++ files/wordpress/run.sh
@@ -2,13 +2,20 @@
 
 for i in $(seq 30)
 do
-	if ! mysqladmin -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST ping 2> /dev/null; then
+	if ! mysqladmin -u $MYSQL_USER -p$MYSQL_USER_PASSWORD -h $MYSQL_HOST ping 2> /dev/null; then
+		printf "Wait for mariadb server... ($i sec)\n"
 		sleep 1
 	else
 		break
 	fi
 done
 
-wp --allow-root --path=/var/www/wordpress config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
+wp core download --locale=ko_KR --allow-root
 
-/bin/sh
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASSWORD --dbhost=$MYSQL_HOST --allow-root
+
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+
+php-fpm7 -F
