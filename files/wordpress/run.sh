443d60a2baae2de4529f4eecd5d195501ef57242
SeungJoonChoi <tmdwns0845@naver.com>
2023-02-10 20:13:37 +0900

    230210

--- files/wordpress/run.sh
+++ files/wordpress/run.sh
@@ -2,14 +2,14 @@
 
 # php-fpm config
 # php-fpm.config includes www.config file
-# edit 'user', 'owner' and 'group' in www.config file
-
+# Edits 'user', 'owner' and 'group' in www.config file.
 if cat /etc/php7/php-fpm.d/www.conf | grep -q "user = nobody"; then
 	sed -i "s/.*user = nobody.*/user = nginx/g" /etc/php7/php-fpm.d/www.conf
 	sed -i "s/.*group = nobody.*/group = nginx/g" /etc/php7/php-fpm.d/www.conf
 	sed -i "s/.*listen = 127.0.0.1.*/listen = 9000/g" /etc/php7/php-fpm.d/www.conf
 fi
 
+# wp-config
 if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
 	for i in $(seq 60)
 	do
@@ -21,13 +21,15 @@ if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
 		fi
 	done
 
+	# Downloads Wordpress core files.
 	wp core download --locale=ko_KR --allow-root
-
+	# Creates wp-config.php.
 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASSWORD --dbhost=$MYSQL_HOST --allow-root
-
+	# Creates the WordPress tables in the database.
 	wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
-
+	# Creates a new user.
 	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
 fi
 
+# Runs php-fpm foreground.
 php-fpm7 -F
