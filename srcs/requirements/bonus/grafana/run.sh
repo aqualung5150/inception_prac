2190dd4be7092e88458b786671cf7d8cc96338fe
SeungJoonChoi <tmdwns0845@naver.com>
2023-02-26 12:13:45 +0900

    Set grafana admin password

--- srcs/requirements/bonus/grafana/run.sh
+++ srcs/requirements/bonus/grafana/run.sh
@@ -1,7 +1,3 @@
 # !/bin/sh
 
-<<<<<<< HEAD
 grafana-server --homepath /usr/share/grafana
-=======
-grafana-server --config /etc/grafana.ini --homepath /usr/share/grafana
->>>>>>> 1ce7164a1463506d57be95e29ca5bbb9e086088b
