86ea3bc641e1b818b73453253b6dd5257cbf4b80
SeungJoonChoi <tmdwns0845@naver.com>
2023-03-13 19:43:16 +0900

    Set redis protected-mode

--- srcs/requirements/bonus/redis/run.sh
+++ srcs/requirements/bonus/redis/run.sh
@@ -1,5 +1,12 @@
 # !/bin/sh
 
+if cat /etc/redis.conf | grep -q "protected-mode yes"; then
+    echo "Set protected-mode."
+    sed -i "s/protected-mode yes/protected-mode no/" /etc/redis.conf
+else
+    echo "Already protected-mode setup."
+fi
+
 if cat /etc/redis.conf | grep -q "bind 127.0.0.1"; then
     echo "Set bind-address."
     sed -i "s/bind 127.0.0.1/bind 0.0.0.0/" /etc/redis.conf
